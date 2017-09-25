4e7881625df1ce78051b2dbcfb50a87b7ee4ed5d
Kevin Gao <kevin.gao@datafox.co>
2017-09-25 15:12:41 -0700

    Add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'script',
+  },
+  extends: 'airbnb-base',
+  env: {
+    node: true,
+  },
+  rules: {
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      // dangling commas in functions are not part of the spec yet
+      functions: 'never',
+    }],
+    'function-paren-newline': 'off',
+    // prevents step-down code style, so disable it for functions and classes
+    'no-use-before-define': ['error', {
+      functions: false,
+      classes: false,
+      variables: true,
+    }],
+    strict: ['error', 'global'],
+  },
+};
