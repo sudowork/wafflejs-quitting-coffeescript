58582622ef91cf7a68bce5f4b30816d0412d88a0
Kevin Gao <kevin.gao@datafox.co>
2017-10-04 11:10:54 -0700

    Built from 7dc58ce on branch master

--- js/charts.js
+++ js/charts.js
@@ -1,4 +1,4 @@
-/* global Highcharts, Reveal, onLoad */
+/* global Highcharts, onLoad */
 
 // eslint-disable-next-line func-names
 (function initCharts() {
@@ -550,50 +550,9 @@
     return data.map(row => row[seriesName]);
   }
 
-  function initBulkDecaffeinateDemoWhenReady() {
-    let alreadyLoaded = false;
-    if (isDemoSlideInVicinity(Reveal.getState().indexh)) {
-      alreadyLoaded = true;
-      initBulkDecaffeinateDemo();
-      return;
-    }
-    Reveal.addEventListener('slidechanged', ({ indexh: currentSlideIndex }) => {
-      if (alreadyLoaded) return;
-      if (isDemoSlideInVicinity(currentSlideIndex)) {
-        alreadyLoaded = true;
-        initBulkDecaffeinateDemo();
-      }
-    });
-  }
-
-  function isDemoSlideInVicinity(currentSlideIndex) {
-    const isDemo = slide => !!slide.querySelector('#bulk-decaffeinate-demo');
-    const currentSlide = Reveal.getSlide(currentSlideIndex);
-    const previousSlide = Reveal.getSlide(currentSlideIndex - 1);
-    const nextSlide = Reveal.getSlide(currentSlideIndex + 1);
-    return (
-      (isDemo(currentSlide)) ||
-      (previousSlide && isDemo(previousSlide)) ||
-      (nextSlide && isDemo(nextSlide))
-    );
-  }
-
-  function initBulkDecaffeinateDemo() {
-    const script = document.createElement('script');
-    script.src = 'https://asciinema.org/a/NJRer9YOAOiuKZPZG6TY2uuS3.js';
-    script.setAttribute('data-theme', 'solarized-light');
-    script.setAttribute('data-speed', '2.5');
-    script.setAttribute('data-cols', '125');
-    script.setAttribute('data-rows', '35');
-    script.id = 'asciicast-NJRer9YOAOiuKZPZG6TY2uuS3';
-    script.async = true;
-    document.getElementById('bulk-decaffeinate-demo').appendChild(script);
-  }
-
   onLoad(() => useTheme(HC_ELEMENTARY_THEME));
   onLoad(initCaffeineConsumptionChartBefore);
   onLoad(initPullRequestChart);
-  onLoad(initCaffeineConsumptionChartAfter);
   onLoad(initLinesOfCodeChart);
-  onLoad(initBulkDecaffeinateDemoWhenReady);
+  onLoad(initCaffeineConsumptionChartAfter);
 }());
